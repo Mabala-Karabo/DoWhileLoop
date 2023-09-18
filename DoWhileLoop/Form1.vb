d125b0ffeff8c69e79facbe965c26d0f124f7d83
Mabala-Karabo <mabalakarabo25@gmail.com>
2023-09-18 16:56:57 +0200

    Add project files.

---
+++ DoWhileLoop/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim x As Integer = 1
+
+        Do While (x < 10)
+            MsgBox("The value of X is " & x)
+            x += 1
+        Loop
+        ' the following code is the same as the prev ones 
+        ' Do While Not (x > 10)
+        'MsgBox("The value of X is " & x)
+        'x += 1
+        'Loop
+    End Sub
+End Class
